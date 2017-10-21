57922edf5a62523c0c90a173e4460f887a970305
Niall Eccles <n.eccles14@gmail.com>
2017-10-21 16:13:09 +0100

    Added in recipe item

--- recipe/src/app/app.module.ts
+++ recipe/src/app/app.module.ts
@@ -9,12 +9,24 @@ import { MatChipsModule } from '@angular/material';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material';
 import { MatGridListModule } from '@angular/material';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeDetailComponent,
   ],
   imports: [
     BrowserModule,
---
+++ recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: '.recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe ('A test recipe', 'this is a test', 'https://images.unsplash.com/photo-1485963631004-f2f00b1d6606?dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
