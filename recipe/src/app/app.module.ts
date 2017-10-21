33c3da3ee7d739aef2b9cc5c4f6f5239ca9c952d
Niall Eccles <n.eccles14@gmail.com>
2017-10-21 13:27:59 +0100

    Added new brochure test

--- recipe/src/app/app.module.ts
+++ recipe/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { MatCardModule } from '@angular/material';
 import { MatChipsModule } from '@angular/material';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatChipsModule
+    MatChipsModule,
+    MatToolbarModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
