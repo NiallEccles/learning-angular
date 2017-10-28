905f8f3b6823512e17ff09a17cf0813df0e25348
Niall Eccles <n.eccles14@gmail.com>
2017-10-29 00:43:01 +0100

    Updated recipes app. Very basic "routing" and updates to showing recipes depending on what item is clicked.

--- recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,14 +8,20 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe ('A test recipe', 'this is a test', 'https://images.unsplash.com/photo-1485963631004-f2f00b1d6606?dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb'),
-    new Recipe ('A test recipe', 'this is a test', 'https://images.unsplash.com/photo-1485963631004-f2f00b1d6606?dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb')
+    new Recipe ('A test recipe', 'this is a test', 'https://images.unsplash.com/photo-1490474418585-ba9bad8fd0ea?auto=format&fit=crop&w=750&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
