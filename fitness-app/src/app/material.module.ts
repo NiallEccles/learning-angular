d1ee5821383ae62da4bba9d49c6514ba3c0d21e6
Niall Eccles <n.eccles14@gmail.com>
2018-01-27 22:39:29 +0000

    Added in datepicker.

--- fitness-app/src/app/material.module.ts
+++ fitness-app/src/app/material.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { 
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule 
+} from '@angular/material';
 
 @NgModule({
     imports: [
         MatButtonModule,
         MatIconModule,
         MatFormFieldModule,
-        MatInputModule
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule
     ],
     exports: [
         MatButtonModule,
         MatIconModule,
         MatFormFieldModule,
-        MatInputModule
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule
     ],
 
 })
