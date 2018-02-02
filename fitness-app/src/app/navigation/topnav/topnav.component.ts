a55816a7141d6c2fa73734615734fc255457731f
Niall Eccles <n.eccles14@gmail.com>
2018-02-02 21:33:06 +0000

    Added in redirects.

--- fitness-app/src/app/navigation/topnav/topnav.component.ts
+++ fitness-app/src/app/navigation/topnav/topnav.component.ts
@@ -24,6 +24,10 @@ export class TopnavComponent implements OnInit, OnDestroy {
     this.sidenavToggle.emit();
   }
 
+  onLogout(){
+    this.authService.logout();
+  }
+
   ngOnDestroy(){
     this.authSubscription.unsubscribe();
   }
